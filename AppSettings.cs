9ccbe95298b99f44b08c6eee940c97ee82b478c1
kiaanonthehub <kiaanmaharaj7@gmail.com>
2021-08-28 18:06:42 +0200

    Add project files.

---
+++ AppSettings.cs
@@ -0,0 +1,18 @@
+namespace CosmosTableSamples
+{
+    using Microsoft.Extensions.Configuration;
+
+    public class AppSettings
+    {
+        public string StorageConnectionString { get; set; }
+
+        public static AppSettings LoadAppSettings()
+        {
+            IConfigurationRoot configRoot = new ConfigurationBuilder()
+                .AddJsonFile("Settings.json")
+                .Build();
+            AppSettings appSettings = configRoot.Get<AppSettings>();
+            return appSettings;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CosmosTableSamples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Azure Cosmos Table Samples");
+            BasicSamples basicSamples = new BasicSamples();
+            basicSamples.RunSamples().Wait();
+
+            //AdvancedSamples advancedSamples = new AdvancedSamples();
+            //advancedSamples.RunSamples().Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+    }
+}
+
+/*
+ * ICE 2: Microsoft documentation
+ * Source code available at : https://docs.microsoft.com/en-us/azure/cosmos-db/table/tutorial-develop-table-dotnet
+ * 
+ * Extensions (PMC): 
+ *Install-Package Microsoft.Azure.Cosmos.Table -Version 1.0.8
+ *Install-Package Microsoft.Extensions.Configuration -Version 5.0.0
+ *Install-Package Microsoft.Extensions.Configuration.Json -Version 5.0.0
+ *Install-Package Microsoft.Extensions.Configuration.Binder -Version 5.0.0
+ */
